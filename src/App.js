a35cac46346fdf491f211c94dcf28c0af9f4001f
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-07-10 18:38:00 +0500

    Finish first iteration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import {Cards,Chart,CountryPicker} from './components'
+import styles from './App.module.css';
+import {fetchData} from './api';
+
 
 function App() {
+  const [data, setdata] = useState({});
+  const [country,setCountry] = useState("");
+  
+  const handleCountryChange  = async(country)=>{
+    const data  = await fetchData(country);
 
-  return (
-    <div>
-      
-    </div>
-  );
+    setCountry(country);
+    setdata(data);
+  }
+
+  useEffect(()=>{
+
+    async function GetData(){
+      const data = await fetchData();
+      setdata(data);
+    }
+    GetData();
+  },[])
+    return (
+        <div className={styles.container}>
+          <Cards data={data}/>
+          <CountryPicker handleCountryChange = {handleCountryChange}/> 
+          <Chart data={data} country={country}/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Card,Typography,CardContent,Grid, StylesProvider} from '@material-ui/core';
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+
+function Cards({data:{confirmed,recovered,deaths,lastUpdate}}) {  
+    if(!confirmed){
+        return "Loading ...."
+    }
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component = {Card} className={cx(styles.card, styles.infected)} xs={12} md={3}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                            <Typography varient="h5">
+                                <CountUp start={0} end={confirmed.value} duration={2.5} separator={","}/>
+                            </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography varient="body2">Number of Active Cases</Typography>
+                    </CardContent>
+
+                </Grid>
+
+                <Grid item component = {Card} className={cx(styles.card, styles.recovered)} xs={12} md={3}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography varient="h5">
+                                <CountUp start={0} end={recovered.value} duration={2.5} separator={","}/>
+                            </Typography>
+                            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography varient="body2">Number of Recovered Cases</Typography>
+                    </CardContent>
+
+                </Grid>
+
+                <Grid item component = {Card} className={cx(styles.card, styles.deaths)} xs={12} md={3}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography varient="h5">
+                                <CountUp start={0} end={deaths.value} duration={2.5} separator={","}/>
+                            </Typography>
+                            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography varient="body2">Number of Death Cases</Typography>
+                    </CardContent>
+
+                </Grid>
+            </Grid>
+
+        </div>
+    )
+}
+
+export default Cards
